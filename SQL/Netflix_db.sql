39c934fb2af1eaf734444badec41f62931546349
Miguel Angel Sandoval Sanchez <20470266@campeche.tecnm.mx>
2026-02-19 23:51:25 -0600

    Agregar la información a la base de datos de netflix_db

--- SQL/Netflix_db.sql
+++ SQL/Netflix_db.sql
@@ -1,30 +1,31 @@
-create database if not exists netflix_db;
---exitencion para uuid
-create extension if not exists pgcrypto;
+-- Active: 1771307265500@@127.0.0.1@5432@netflix_db
+create database netflix_db;
 --tabla de peliculas
+drop table users;
 create table movies (
-    movieID varchar(8) primary key,
-    title varchar(100) not null,
-    releaseDate date not null,
-    genre varchar(100) not null,
-    participantName varchar(100) not null,
-    roleParticipant varchar(100) not null,
-    awardMovie varchar(50) not null 
+    "movieID" varchar(8) primary key,
+    "title" varchar(100) not null,
+    "releaseDate" date not null,
+    "gender" varchar(100) not null,
+    "participantName" varchar(100) not null,
+    "roleParticipant" varchar(100) not null,
+    "awardMovie" varchar(50) not null 
 );
 --tabla de usuarios
 create table users(
-    userID uuid primary key default gen_random_uuid(),
-    userName varchar(100) not null,
-    country varchar(100) not null,
-    subscription varchar(100) not null
+    "userID" int primary key,
+    "userName" varchar(100) not null,
+    "country" varchar(100) not null,
+    "subscription" varchar(100) not null
 );
 --tabla de observaciones
 create table observations(
     
-    userID uuid not null,
-    movieID varchar(8) not null,
-    rating decimal(3,1),
-    timestamp timestamp,
-    constraint fk_observations_movie foreign key (movieID) references movies(movieID),
-    constraint fk_observations_user foreign key (userID) references users(userID)
+    "userID" int not null,
+    "movieID" varchar(8) not null,
+    "rating" decimal(3,1),
+    "timestamp" timestamp,
+    constraint fk_observations_movie foreign key ("movieID") references movies("movieID"),
+    constraint fk_observations_user foreign key ("userID") references users("userID")
 );
+SELECT * from users;
